2346f2a35e0e7fe9f285c2d4c9681f8d9f334ffd
glowfi <86939591+glowfi@users.noreply.github.com>
2021-11-21 19:42:49 +0530

    added a small fix

--- 1_pacstrap.sh
+++ 1_pacstrap.sh
@@ -100,7 +100,7 @@ case $formatdisk in
 
     # GO TO MAIN SYSTEM
 
-    arch-chroot /mnt /bin/bash -c "git clone https://github.com/glowfi/setup;chmod +x /setup/2_after_pacstrap.sh;/setup/2_after_pacstrap.sh $1 $2;rm -rf setup;"
+    arch-chroot /mnt /bin/bash -c "git clone https://github.com/glowfi/setup;chmod +x /setup/2_after_pacstrap.sh;/setup/2_after_pacstrap.sh $1 '$2';rm -rf setup;"
     ;;
 
     n|N|no|No|NO)
