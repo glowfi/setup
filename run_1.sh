ba9b2187f46068a8b8a0c763c2528f0a77488bfd
glowfi <86939591+glowfi@users.noreply.github.com>
2021-11-21 18:55:40 +0530

    added logic for dynamic disk partioning

--- run_1.sh
+++ run_1.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 sh setup/0_welcome.sh 
-sh setup/1_pacstrap.sh
+
+echo "What would be the username?"
+read uname
+echo "What would be the fullname of the user?"
+read fname
+
+sh setup/1_pacstrap.sh $uname $fname
