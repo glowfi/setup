0f5159423117bbc74cdafcfe7588c54dd8f21d6b
glowfi <86939591+glowfi@users.noreply.github.com>
2021-11-30 13:36:07 +0530

    some dwm fixes

--- configs/dwm-6.2/config.def.h
+++ configs/dwm-6.2/config.def.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#32302f";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#98971a";
+static const char col_cyan[]        = "#477D6F";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
