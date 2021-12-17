3b9aa426446a9d122ea7bef88e43805e644e2348
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-17 21:12:05 +0530

    added lua config to nvim

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -108,6 +108,22 @@ require'lspconfig'.rust_analyzer.setup {
     end
 }
 
+-- Lua LSP 
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+    settings = {
+        Lua = {
+            runtime = {version = 'LuaJIT', path = runtime_path},
+            diagnostics = {globals = {'vim'}},
+            workspace = {library = vim.api.nvim_get_runtime_file("", true)},
+            telemetry = {enable = false}
+        }
+    }
+}
+
 -- HTML CSS
 require'lspconfig'.html.setup {capabilities = capabilities}
 
