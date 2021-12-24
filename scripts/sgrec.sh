39995d0c164b85a299420fb47611f2238f61ecef
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-24 19:27:11 +0530

    added script

--- scripts/sgrec.sh
+++ scripts/sgrec.sh
@@ -1,7 +1,52 @@
 #!/bin/bash
 
+mic="no"
+file="$1"
+
+usage() {
+	cat <<EOF
+    -m   | --mic           Record audio or not 
+    -f   | --file          Recognize audio from file 
+    -h   | --help          Prints help 
+
+    ## EXAMPLE (To recognize from microphone)
+    sgrec.sh --mic "yes"
+
+    ## EXAMPLE (To recognize from file)
+    sgrec.sh --file audio.mp3
+
+EOF
+}
+
+while [[ $# > 0 ]]; do
+	case "$1" in
+
+	-m | --mic)
+		mic="$2"
+		shift
+		;;
+
+	-f | --file)
+		file="$2"
+		shift
+		;;
+
+	--help | *)
+		usage
+		exit 1
+		;;
+	esac
+	shift
+done
+
+if [[ "$mic" = "yes" ]]; then
+	echo "Listening with you microphone ....."
+	setsid songrec
+	exit 1
+fi
+
 echo "Recognizing ....."
-output=$(songrec audio-file-to-recognized-song "$1" | jq -r "[.track.title,.track.subtitle,.track.share.image] | @csv" | awk -F, '{print $1 "\n" $2 "\n" $3}')
+output=$(songrec audio-file-to-recognized-song "$file" | jq -r "[.track.title,.track.subtitle,.track.share.image] | @csv" | awk -F, '{print $1 "\n" $2 "\n" $3}')
 
 songName=$(echo "$output" | head -1 | tr -d '"')
 artistName=$(echo "$output" | head -2 | tail -1 | tr -d '"')
