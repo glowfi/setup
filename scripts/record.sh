037ef5edc4e11d2868388db488c22a07e4230d52
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-24 21:17:35 +0530

    small fix

--- scripts/record.sh
+++ scripts/record.sh
@@ -4,7 +4,7 @@ INRES="1366x768"
 OUTRES="1366x768"
 FPS="30"
 
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -ac 2 \
--i default -vcodec libx264 -s "$OUTRES" \
--acodec libmp3lame -ab 128k -ar 44100 \
--threads 0 -f flv $1
+ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -ac 1 \
+	-i default -vcodec libx264 -s "$OUTRES" \
+	-acodec libmp3lame -ab 128k -ar 44100 \
+	-threads 0 -f flv $1
