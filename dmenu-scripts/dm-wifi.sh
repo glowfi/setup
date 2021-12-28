16718ec3fb17cc49b2ecc0562121a996949481b1
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-28 21:19:08 +0530

    added a fix for dm-wifi

--- dmenu-scripts/dm-wifi.sh
+++ dmenu-scripts/dm-wifi.sh
@@ -4,12 +4,18 @@ choice=$(printf "Connect\nDisconnect" | dmenu -p "Select:" -i -nb "#32302f" -nf 
 
 if [[ "$choice" = "Connect" ]]; then
 	bssid=$(nmcli device wifi list | sed -n '1!p' | cut -b 9- | dmenu -p "Select Wifi 📶:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -l 10 | cut -d " " -f1)
-	password=$(echo >/dev/null | dmenu -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -p "Password 🔑:")
-	nmcli device wifi connect "$bssid" password "$password"
-	notify-send "Connected to wifi!"
+	if [[ "$bssid" ]]; then
+		password=$(echo >/dev/null | dmenu -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -p "Password 🔑:")
+		if [[ "$password" ]]; then
+			nmcli device wifi connect "$bssid" password "$password"
+			notify-send "Connected to wifi!"
+		fi
+	fi
 
 elif [[ "$choice" = "Disconnect" ]]; then
 	currentNetwork=$(iwgetid -r)
-	nmcli con down id "$currentNetwork"
-	notify-send "Disconnected from wifi!"
+	if [[ "$currentNetwork" ]]; then
+		nmcli con down id "$currentNetwork"
+		notify-send "Disconnected from wifi!"
+	fi
 fi
