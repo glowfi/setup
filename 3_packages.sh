6149483dacd798be54c15530c6a8575553c5fbba
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-30 17:47:49 +0530

    added a package

--- 3_packages.sh
+++ 3_packages.sh
@@ -91,7 +91,7 @@ cd ..
 rm -rf tty-clock
 
 ### EDIT
-sudo pacman -S --noconfirm gimp kdenlive ffmpeg ffmpegthumbnailer youtube-dl mpv songrec ardour mediainfo
+sudo pacman -S --noconfirm gimp kdenlive ffmpeg ffmpegthumbnailer youtube-dl mpv songrec ardour mediainfo stellarium
 yay -S --noconfirm gimp-plugin-registry
 
 # ENABLE ZRAM
