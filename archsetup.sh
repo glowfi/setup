d3c8300dd5d8e03b412ed82881a191cea7f3ef85
glowfi <86939591+glowfi@users.noreply.github.com>
2022-01-19 01:20:54 +0530

    testv5

--- archsetup.sh
+++ archsetup.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-# Find the name of the folder the scripts are in
 setfont ter-v22b
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-CONFIG_FILE=$SCRIPT_DIR/setup.conf
-
 # Read Username
 USERNAME=$(sed -n '6p' <"$CONFIG_FILE")
+
+# Find the name of the folder the scripts are in
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Run scripts
 bash $SCRIPT_DIR/0_startup.sh
 bash $SCRIPT_DIR/1_pacstrap.sh
 arch-chroot /mnt /bin/bash -c "./setup/2_after_pacstrap.sh"
-arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- ./setup/3_packages.sh
+arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/setup/3_packages.sh
