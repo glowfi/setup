e9c88175f258ce6109e23afd880ff6f989ea5cb0
glowfi <86939591+glowfi@users.noreply.github.com>
2022-01-21 16:45:58 +0530

    final test

--- run_1.sh
+++ run_1.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-sh setup/0_welcome.sh
-sh setup/1_pacstrap.sh
+# Find the name of the folder the scripts are in
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Run scripts
+bash $SCRIPT_DIR/0_startup.sh || exit 0
+bash $SCRIPT_DIR/1_pacstrap.sh || exit 0
+arch-chroot /mnt /bin/bash -c "./setup/2_after_pacstrap.sh" || exit 0
