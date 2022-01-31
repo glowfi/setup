643597e1a382318b321d7083bf964066df4bb68b
glowfi <86939591+glowfi@users.noreply.github.com>
2022-01-31 23:26:22 +0530

    added a script

---
+++ scripts/secr.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Dependencies
+sudo pacman -S --noconfirm pass
+
+# Generate GPG KEY
+gpg --full-generate-key
+key=$(gpg --list-secret-keys --keyid-format long | tail -4 | head -1 | xargs)
+pass init "$key"
+gpg-connect-agent reloadagent /bye
