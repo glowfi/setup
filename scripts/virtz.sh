659289031b118e187f2944577070b49ae5d82772
glowfi <86939591+glowfi@users.noreply.github.com>
2022-02-05 02:31:36 +0530

    added some script fixes

--- scripts/virtz.sh
+++ scripts/virtz.sh
@@ -3,7 +3,10 @@
 # VIRTUALIZATION SUPPORT
 
 #--INSTALL
-# sudo pacman -S dnsmasq virt-manager qemu ebtables edk2-ovmf;sudo usermod -G libvirt -a john;sudo systemctl start libvirtd
+yes | sudo pacman -S dnsmasq virt-manager qemu ebtables edk2-ovmf
+sudo usermod -G libvirt -a "$USER"
+sudo systemctl start libvirtd
 
 #--UNINSTALL
-# sudo pacman -Rns dnsmasq virt-manager qemu edk2-ovmf;sudo gpasswd -d john libvirt
+sudo pacman -Rns dnsmasq virt-manager qemu edk2-ovmf
+sudo gpasswd -d "$USER" libvirt
