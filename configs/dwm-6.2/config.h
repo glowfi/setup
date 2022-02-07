3f2956aa5888ab6c1a181d988ecf9f28b282e6c9
glowfi <86939591+glowfi@users.noreply.github.com>
2022-02-07 17:28:28 +0530

    added dwm fix

--- configs/dwm-6.2/config.h
+++ configs/dwm-6.2/config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fantasque Sans Mono Bold:size=10","JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Fantasque Sans Mono Bold:size=13","JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#32302f";
 static const char col_gray2[]       = "#444444";
