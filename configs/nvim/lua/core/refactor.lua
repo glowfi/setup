228346836a3ef33c82c7b770e367a7c096562b0f
glowfi <86939591+glowfi@users.noreply.github.com>
2022-02-10 18:32:03 +0530

    fixed refactor module nvim

--- configs/nvim/lua/core/refactor.lua
+++ configs/nvim/lua/core/refactor.lua
@@ -2,31 +2,15 @@
 local status_ok, refactor = pcall(require, "refactoring")
 if not status_ok then return end
 
-refactor.setup()
+refactor.setup({})
 
-local function refactor(prompt_bufnr)
-    local content = require("telescope.actions.state").get_selected_entry(
-                        prompt_bufnr)
-    require("telescope.actions").close(prompt_bufnr)
-    require("refactoring").refactor(content.value)
-end
+-- Load refactoring Telescope extension
+require("telescope").load_extension("refactoring")
 
-M = {}
-M.refactors = function()
-    local opts = require("telescope.themes").get_cursor() -- set personal telescope options
-    require("telescope.pickers").new(opts, {
-        prompt_title = "refactors",
-        finder = require("telescope.finders").new_table({
-            results = require("refactoring").get_refactors()
-        }),
-        sorter = require("telescope.config").values.generic_sorter(opts),
-        attach_mappings = function(_, map)
-            map("i", "<CR>", refactor)
-            map("n", "<CR>", refactor)
-            return true
-        end
-    }):find()
-end
+-- Remap to open the Telescope refactoring menu in visual mode
+vim.api.nvim_set_keymap("v", "<leader>rr",
+                        "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+                        {noremap = true})
 
 -- Keymappings
 vim.api.nvim_set_keymap("v", "<Leader>re",
@@ -35,5 +19,9 @@ vim.api.nvim_set_keymap("v", "<Leader>re",
 vim.api.nvim_set_keymap("v", "<Leader>rf",
                         [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
                         {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<Leader>rt", [[ <Esc><Cmd>lua M.refactors()<CR>]],
+vim.api.nvim_set_keymap("v", "<Leader>rv",
+                        [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
+                        {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("v", "<Leader>ri",
+                        [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
                         {noremap = true, silent = true, expr = false})
