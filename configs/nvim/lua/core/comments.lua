30e9809e87ac0186f91d2d5b0e615e3e4c77c471
glowfi <86939591+glowfi@users.noreply.github.com>
2022-02-25 16:01:46 +0530

    prettified nvim

--- configs/nvim/lua/core/comments.lua
+++ configs/nvim/lua/core/comments.lua
@@ -1,16 +1,16 @@
 -- Settings
 local status_ok, comments = pcall(require, "nvim_comment")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 comments.setup({
-    comment_empty = false,
-    hook = function()
-        require("ts_context_commentstring.internal").update_commentstring()
-    end
+	comment_empty = false,
+	hook = function()
+		require("ts_context_commentstring.internal").update_commentstring()
+	end,
 })
 
 -- Keymappings
-vim.api.nvim_set_keymap('n', '<C-_>', ":CommentToggle<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<C-_>', ":CommentToggle<CR>",
-                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-_>", ":CommentToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-_>", ":CommentToggle<CR>", { noremap = true, silent = true })
--- configs/nvim/lua/core/dashboard.lua
+++ configs/nvim/lua/core/dashboard.lua
@@ -1,41 +1,40 @@
 -- Settings
 -- CUSTOM HEADER
 local custom_header = {
-    '               ,   ,                             ',
-    '               $,  $,     ,                      ',
-    '               "ss.$ss. .s"                      ',
-    '       ,     .ss$$$$$$$$$$s,                     ',
-    '       $. s$$$$$$$$$$$$$$`$$Ss                   ',
-    '       "$$$$$$$$$$$$$$$$$$o$$$       ,           ',
-    '      s$$$$$$$$$$$$$$$$$$$$$$$$s,  ,s            ',
-    '     s$$$$$$$$$"$$$$$$""""$$$$$$"$$$$$,          ',
-    '     s$$$$$$$$$$s""$$$$ssssss"$$$$$$$$"          ',
-    '    s$$$$$$$$$$"         `"""ss"$"$s""           ',
-    '    s$$$$$$$$$$,              `"""""$  .s$$s     ',
-    '    s$$$$$$$$$$$$s,...               `s$$"  `    ',
-    '`ssss$$$$$$$$$$$$$$$$$$$$####s.     .$$"$.   , s-',
-    '  `""""$$$$$$$$$$$$$$$$$$$$#####$$$$$$"     $.$" ',
-    '        "$$$$$$$$$$$$$$$$$$$$$####s""     .$$$|  ',
-    '         "$$$$$$$$$$$$$$$$$$$$$$$$##s    .$$" $  ',
-    '          $$""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"   ` ',
-    '         $$"  "$"$$$$$$$$$$$$$$$$$$$$S"""""      ',
-    '        ,"     $  $$$$$$$$$$$$$$$$####s          ',
-    '                .s$$$$$$$$$$$$$$$$$####"         ',
-    '              $$$$$$$$$$$$$$$$$$$$####"          '
+	"               ,   ,                             ",
+	"               $,  $,     ,                      ",
+	'               "ss.$ss. .s"                      ',
+	"       ,     .ss$$$$$$$$$$s,                     ",
+	"       $. s$$$$$$$$$$$$$$`$$Ss                   ",
+	'       "$$$$$$$$$$$$$$$$$$o$$$       ,           ',
+	"      s$$$$$$$$$$$$$$$$$$$$$$$$s,  ,s            ",
+	'     s$$$$$$$$$"$$$$$$""""$$$$$$"$$$$$,          ',
+	'     s$$$$$$$$$$s""$$$$ssssss"$$$$$$$$"          ',
+	'    s$$$$$$$$$$"         `"""ss"$"$s""           ',
+	'    s$$$$$$$$$$,              `"""""$  .s$$s     ',
+	'    s$$$$$$$$$$$$s,...               `s$$"  `    ',
+	'`ssss$$$$$$$$$$$$$$$$$$$$####s.     .$$"$.   , s-',
+	'  `""""$$$$$$$$$$$$$$$$$$$$#####$$$$$$"     $.$" ',
+	'        "$$$$$$$$$$$$$$$$$$$$$####s""     .$$$|  ',
+	'         "$$$$$$$$$$$$$$$$$$$$$$$$##s    .$$" $  ',
+	'          $$""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"   ` ',
+	'         $$"  "$"$$$$$$$$$$$$$$$$$$$$S"""""      ',
+	'        ,"     $  $$$$$$$$$$$$$$$$####s          ',
+	'                .s$$$$$$$$$$$$$$$$$####"         ',
+	'              $$$$$$$$$$$$$$$$$$$$####"          ',
 }
 
 -- CUSTOM FOOTER
-local handle = io.popen(
-                   "while fortune -sn80 |awk 'END { if (NR == 1) { print; exit 1 } }'; do true; done")
+local handle = io.popen("while fortune -sn80 |awk 'END { if (NR == 1) { print; exit 1 } }'; do true; done")
 local var = handle:read("*a")
 handle:close()
 local s = var:sub(1, -2)
-local custom_footer = {s}
+local custom_footer = { s }
 
 -- SETTING CUSTOM HEADER AND FOOTER
 vim.g.dashboard_custom_header = custom_header
 vim.g.dashboard_custom_footer = custom_footer
 
 -- CHANGE START SCREEN
-vim.g.dashboard_default_executive = 'telescope'
-vim.g.dashboard_custom_section = {a = {description = {''}, command = ''}}
+vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_custom_section = { a = { description = { "" }, command = "" } }
