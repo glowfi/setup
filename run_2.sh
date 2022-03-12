12586666264cce4bee3658f4be28b0d40cacc1f8
glowfi <86939591+glowfi@users.noreply.github.com>
2022-03-12 19:21:20 +0530

    minute fix

--- run_2.sh
+++ run_2.sh
@@ -27,13 +27,13 @@ chmod +x ~/setup/4_cdx.sh
 ~/setup/4_cdx.sh
 
 if [[ $choice == "1" ]]; then
-    chmod +x ~/setup/5_kde_.sh
-    ~/setup/5_kde_.sh
+	chmod +x ~/setup/5_kde_.sh
+	~/setup/5_kde_.sh
 elif [[ $choice == "2" ]]; then
-    chmod +x ~/setup/5_dwm_.sh
-    ~/setup/5_dwm_.sh $uname
+	chmod +x ~/setup/5_dwm_.sh
+	~/setup/5_dwm_.sh $uname
 else
-  echo "Wrong choice!"
+	echo "Wrong choice!"
 fi
 
 ## CLEANUP
@@ -44,14 +44,14 @@ echo "--------------Cleaning up...-------------------------"
 echo "-----------------------------------------------------"
 echo ""
 
-yes | sudo pacman -Sc;
-yes | yay -Sc;
-printf "Cleaned Unused Pacakges!\n";
+yes | sudo pacman -Sc
+yes | yay -Sc
+printf "Cleaned Unused Pacakges!\n"
 
-rm -rf ~/.cache/*;
-printf "Cleaned Cache!\n";
+sudo rm -rf ~/.cache/*
+printf "Cleaned Cache!\n"
 
-sudo pacman -Rns $(pacman -Qtdq)  2> /dev/null;
+sudo pacman -Rns $(pacman -Qtdq) 2>/dev/null
 yes | printf "Cleaned Orphans!"
 
 ## DELETE CACHED PASSWORD
@@ -66,16 +66,15 @@ echo "-----------------------------------------------------------"
 echo ""
 
 cd
-rm -rf setup 
+rm -rf setup
 git clone https://github.com/glowfi/setup
-
 
 ## END
 
 echo ""
-echo "   ▄▄   ▀▀█    ▀▀█           ▄▄▄▄                          ▄   " 
-echo "   ██     █      █           █   ▀▄  ▄▄▄   ▄ ▄▄    ▄▄▄     █   " 
-echo "  █  █    █      █           █    █ █▀ ▀█  █▀  █  █▀  █    █   " 
-echo "  █▄▄█    █      █           █    █ █   █  █   █  █▀▀▀▀    ▀   " 
-echo " █    █   ▀▄▄    ▀▄▄         █▄▄▄▀  ▀█▄█▀  █   █  ▀█▄▄▀    █   " 
+echo "   ▄▄   ▀▀█    ▀▀█           ▄▄▄▄                          ▄   "
+echo "   ██     █      █           █   ▀▄  ▄▄▄   ▄ ▄▄    ▄▄▄     █   "
+echo "  █  █    █      █           █    █ █▀ ▀█  █▀  █  █▀  █    █   "
+echo "  █▄▄█    █      █           █    █ █   █  █   █  █▀▀▀▀    ▀   "
+echo " █    █   ▀▄▄    ▀▄▄         █▄▄▄▀  ▀█▄█▀  █   █  ▀█▄▄▀    █   "
 echo ""
