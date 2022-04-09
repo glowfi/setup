effb4037df0e1b4715eff0998723b79a3adae02e
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-09 15:44:50 +0530

    more pywall updates

--- configs/config.fish
+++ configs/config.fish
@@ -351,6 +351,15 @@ end
 
 function setWall
     feh --bg-fill $argv[1]
+    cd
+    cat .fehbg | tail -1 | awk '{print $NF}' | awk -F/ '{print $5}' | tr -d "'" | xargs -I {} wal -s -q -t --backend haishoku -i ~/wall/{}
+    sed -i '9,11d' ~/.cache/wal/colors-wal-dwm.h
+    sed -i 14d ~/.cache/wal/colors-wal-dwm.h
+    cd ~/.config/dwm/
+    cp config.def.h config.h
+    make
+    cd
+    xdotool key super+shift+q
 end
 
 function trad
