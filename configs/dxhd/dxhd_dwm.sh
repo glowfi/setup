c642b5e1d8a80c0d55525a0de7726ab655d6657c
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-12 00:16:01 +0530

    dxhd update fix

--- configs/dxhd/dxhd_dwm.sh
+++ configs/dxhd/dxhd_dwm.sh
@@ -57,8 +57,8 @@ cd
 cat .fehbg | tail -1 | awk '{print $NF}' | awk -F"/" '{print $5}' | tr -d "'" | xargs -I {} wal -s -q -t --backend haishoku -i ~/wall/{}
 sed -i '9,11d' ~/.cache/wal/colors-wal-dwm.h
 sed -i '14d' ~/.cache/wal/colors-wal-dwm.h
-cd ~/.config/dwm/
-cp config.def.h config.h
+cd ~/.config/dwm-6.2/
+make clean
 make
 cd
 xdotool key super+shift+q
