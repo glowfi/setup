10a28dd56d7a7015a1dcfe5c33fbe0ca0e2a2f9e
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-21 00:47:09 +0530

    fixed wallpaper crash

--- scripts/wall.sh
+++ scripts/wall.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 while true; do
-	feh --bg-fill "$(find ~/wall -type f | shuf -n 1)"
-	cd
-	cat .fehbg | tail -1 | awk '{print $NF}' | awk -F"/" '{print $5}' | tr -d "'" | xargs -I {} wal -s -q -t --backend haishoku -i ~/wall/{}
-	sed -i '9,11d' ~/.cache/wal/colors-wal-dwm.h
-	sed -i '14d' ~/.cache/wal/colors-wal-dwm.h
-	cd ~/.config/dwm-6.2/
-	make clean
-	make
-	cd
+	xdotool key super+z
 	sleep 900s
 done &
