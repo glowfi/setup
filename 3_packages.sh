e5ee8d939cec2ae617922af1ec802a1db92009f8
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-27 18:05:51 +0530

    minor fix

--- 3_packages.sh
+++ 3_packages.sh
@@ -76,7 +76,7 @@ rm -rf tty-clock
 
 ### EDIT
 sudo pacman -Syyy --noconfirm gimp kdenlive ffmpeg ffmpegthumbnailer youtube-dl mpv songrec ardour mediainfo
-# sudo pacman -Syyy --noconfirm gimp kdenlive ffmpeg ffmpegthumbnailer youtube-dl mpv songrec ardour mediainfo stellarium
+yay -S --noconfirm stellarium
 yay -S --noconfirm gimp-plugin-registry
 
 # # ENABLE ZRAM
