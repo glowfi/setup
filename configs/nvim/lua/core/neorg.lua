dab748168369b11675e39b5a483921db60ce98e1
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-27 14:38:30 +0530

    neovim fix

--- configs/nvim/lua/core/neorg.lua
+++ configs/nvim/lua/core/neorg.lua
@@ -1,12 +1,6 @@
 require("neorg").setup({
 	load = {
 		["core.defaults"] = {},
-		["core.norg.concealer"] = {
-			config = {
-				icon_preset = "diamond",
-				markup_preset = "dimmed",
-				dim_code_blocks = false,
-			},
-		},
+		["core.norg.concealer"] = {},
 	},
 })
