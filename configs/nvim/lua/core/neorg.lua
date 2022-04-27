e876e22c049da6efaa1bd5cee74f292bda503e6f
glowfi <86939591+glowfi@users.noreply.github.com>
2022-04-28 00:04:30 +0530

    codeblock highligting nvim

--- configs/nvim/lua/core/neorg.lua
+++ configs/nvim/lua/core/neorg.lua
@@ -1,6 +1,10 @@
+-- Config
 require("neorg").setup({
 	load = {
 		["core.defaults"] = {},
 		["core.norg.concealer"] = {},
 	},
 })
+
+-- CodeBlock Highlighting
+vim.cmd("highlight! NeorgCodeBlock guibg=#3c3836")
