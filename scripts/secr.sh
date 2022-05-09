c002cc1d846d7858d73db03f0e684fa13ac7f732
glowfi <86939591+glowfi@users.noreply.github.com>
2022-05-09 14:29:59 +0530

    added extras

--- scripts/secr.sh
+++ scripts/secr.sh
@@ -8,3 +8,7 @@ gpg --full-generate-key
 key=$(gpg --list-secret-keys --keyid-format long | tail -4 | head -1 | xargs)
 pass init "$key"
 gpg-connect-agent reloadagent /bye
+
+# Extras
+yay -S --noconfirm openrazer-meta polychromatic
+sudo gpasswd -a $USER plugdev
