d6dd248e7a5117f57203d7477038fc885dc07d4d
glowfi <86939591+glowfi@users.noreply.github.com>
2022-05-28 14:35:36 +0530

    updated get script

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 link="$1"
-name=$(echo "$link" | awk -F"/" '{print $NF}') 
-curl "$link" -o ~/"$name"
+fileType="$2"
+
+if [[ "$fileType" == "f" ]]; then
+	name=$(echo "$link" | awk -F"/" '{print $NF}')
+	wget "$link" -O ~/"$name"
+elif [[ "$fileType" == "c" ]]; then
+	name=$(echo "$link" | awk -F"/" '{print $NF}')
+	curl "$link" -o ~/"$name"
+else
+	echo "Provide a fileType! For downloading a file give f or just contens give c"
+fi
