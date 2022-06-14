07ed38f8ed1c8e0143c7516555319ee8dfaed797
glowfi <86939591+glowfi@users.noreply.github.com>
2022-06-14 19:59:56 +0530

    updated rename script

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,13 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-path="$1"
-i=1
-echo "Renaming ..."
-for files in $path*; do
-	filename=$(echo "$files" | awk -F"/" '{print $NF}' | awk -F"." '{print $1}' | xargs)
-	ext=$(echo "$files" | awk -F"/" '{print $NF}' | awk -F"." '{print $2}' | xargs)
-	timestamp=$(date +%s)
-	mv "$files" "$path""$i-img-$timestamp"."$ext"
-	((i = i + 1))
-done
-echo "Done!"
+inputDirectory="$1"
+index=$2
+directoryPath=""
+
+rename() {
+	for f in $directoryPath; do
+		fileName=$(echo "$f" | awk -F "/" '{print $NF}')
+		fileExtension=$(echo "$fileName" | awk -F "." '{print $NF}')
+		mv "$f" "$inputDirectory$index.$fileExtension"
+		((index++))
+	done
+}
+
+if [[ -d "$1" ]]; then
+	checkForwardSlash="${inputDirectory:0-1}"
+	if [[ "$checkForwardSlash" == "/" ]]; then
+		echo "entered 1"
+		inputDirectory="$1"
+		directoryPath="$inputDirectory*.*"
+		rename
+	else
+		echo "entered 2"
+		inputDirectory="$1/"
+		directoryPath="$inputDirectory*.*"
+		rename
+	fi
+else
+	echo "Such Directory does not exists!"
+fi
+
+# path="$1"
+# i=1
+# echo "Renaming ..."
+# for files in $path*; do
+# 	filename=$(echo "$files" | awk -F"/" '{print $NF}' | awk -F"." '{print $1}' | xargs)
+# 	ext=$(echo "$files" | awk -F"/" '{print $NF}' | awk -F"." '{print $2}' | xargs)
+# 	timestamp=$(date +%s)
+# 	mv "$files" "$path""$i-img-$timestamp"."$ext"
+# 	((i = i + 1))
+# done
+# echo "Done!"
