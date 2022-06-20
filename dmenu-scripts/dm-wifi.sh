ecd21b5dce67643c74c3b1f1ed441bdd1725c83a
glowfi <86939591+glowfi@users.noreply.github.com>
2022-06-21 01:25:49 +0530

    fixed dmenu coloring

--- dmenu-scripts/dm-wifi.sh
+++ dmenu-scripts/dm-wifi.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-choice=$(printf "Connect\nDisconnect" | dmenu -p "Select:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -l 10)
+choice=$(printf "Connect\nDisconnect" | dmenu -p "Select:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#676085" -sf "#eeeeee" -l 10)
 
 if [[ "$choice" = "Connect" ]]; then
-	bssid=$(nmcli device wifi list | sed -n '1!p' | cut -b 9- | dmenu -p "Select Wifi 📶:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -l 10 | cut -d " " -f1)
+	bssid=$(nmcli device wifi list | sed -n '1!p' | cut -b 9- | dmenu -p "Select Wifi 📶:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#676085" -sf "#eeeeee" -l 10 | cut -d " " -f1)
 	if [[ "$bssid" ]]; then
-		password=$(echo >/dev/null | dmenu -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" -p "Password 🔑:")
+		password=$(echo >/dev/null | dmenu -i -nb "#32302f" -nf "#bbbbbb" -sb "#676085" -sf "#eeeeee" -p "Password 🔑:")
 		if [[ "$password" ]]; then
 			nmcli device wifi connect "$bssid" password "$password"
 			notify-send "Connected to wifi!"
