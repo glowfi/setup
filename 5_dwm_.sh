b90866933e6d6cedf378a77bc5f69bf3b17e9b54
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-14 19:24:15 +0530

    picom fixed again

--- 5_dwm_.sh
+++ 5_dwm_.sh
@@ -86,7 +86,7 @@ echo "# Resolution
 xrandr --output eDP-1 --mode 1920x1080 &
 
 # Picom
-picom -b -f --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 --experimental-backends &
+picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 --experimental-backends &
 
 # Hotkey daemon
 dxhd -b &
