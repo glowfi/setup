a436d2f50b20f90edeefaa93fc403afafae57625
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-23 07:14:06 +0530

    ADDED PYWAL BACK BUT NOW WITH SPEED

--- configs/dxhd/dxhd_dwm.sh
+++ configs/dxhd/dxhd_dwm.sh
@@ -51,5 +51,17 @@ fish -c "sYT -p "dmenu""
 feh --bg-fill "$(find ~/wall -type f | shuf -n 1)"
 
 ## Favourite Wallpaper
-#super + c
+#super + w
 feh --bg-fill ~/wall/228.png
+
+## Change Colorscheme
+#super + c
+cd
+cat ~/.fehbg | tail -1 | awk '{print $NF}' | awk -F"/" '{print $5}' | tr -d "'" | xargs -I {} wal -s -q -t --backend haishoku -i ~/wall/{}
+sed -i '9,11d' ~/.cache/wal/colors-wal-dwm.h
+sed -i '14d' ~/.cache/wal/colors-wal-dwm.h
+cd ~/.config/dwm-6.2/
+make clean
+make
+cd
+xdotool key super+shift+q
