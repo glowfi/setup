3e53199012ca03be12b69ea3b8a048336a9c3ace
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-25 15:19:28 +0530

    removed pywal

--- configs/dwm-6.2/config.h
+++ configs/dwm-6.2/config.h
@@ -18,12 +18,11 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#458588";
 static const char col_trans[]       = "#FFFFFF";
-#include "/home/ayush/.cache/wal/colors-wal-dwm.h"
-/* static const char *colors[][3]      = { */
+static const char *colors[][3]      = {
    /*               fg         bg         border   */ 
-  /* [SchemeNorm] = { col_gray3, col_gray1 },                                  */
-  /* [SchemeSel]  = { col_gray4, col_cyan,  col_trans  }, */
-/* };   */
+  [SchemeNorm] = { col_gray3, col_gray1 },                                 
+  [SchemeSel]  = { col_gray4, col_cyan,  col_trans  },
+};  
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
