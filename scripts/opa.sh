bd524fb7a40c7acde5d6aa60dfddaceddabdfd01
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-27 14:37:00 +0530

    added opacity fix

--- scripts/opa.sh
+++ scripts/opa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-line_check=$(sed -n '45p' ~/.config/kitty/kitty.conf)
+line_check=$(sed -n '46p' ~/.config/kitty/kitty.conf)
 
 if [[ "$line_check" == "## Tabs" ]]; then
 	cp -r ~/setup/configs/kitty/kitty.conf ~/.config/kitty/
 else
-	sed -i '45,48d' ~/.config/kitty/kitty.conf
+	sed -i '45,49d' ~/.config/kitty/kitty.conf
 fi
