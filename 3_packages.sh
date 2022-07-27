e5f432d4793ef8516741c456597ed1d0696ba558
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-27 05:42:18 +0530

    fixed nn

--- 3_packages.sh
+++ 3_packages.sh
@@ -164,8 +164,8 @@ rm -rf nnn
 mkdir -p .config/nnn/plugins
 cd .config/nnn/plugins/
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
-sed -i '228 a                      --theme=gruvbox-dark --paging=never --style="$BAT_STYLE" "$@" &' ~/.config/nnn/plugins/preview-tui
-sed -i '228d' ~/.config/nnn/plugins/preview-tui
+sed -i '226d' ~/.config/nnn/plugins/preview-tui
+sed -i '225 a                      --theme=gruvbox-dark --paging=never --style="$BAT_STYLE" "$@" &' ~/.config/nnn/plugins/preview-tui
 
 # COPY KITTY SETTINGS
 
