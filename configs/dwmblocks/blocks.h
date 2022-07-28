0049bb3824c14f9ff11357b1412b35c948eec542
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-28 15:42:04 +0530

    dwmblocks updated signals

--- configs/dwmblocks/blocks.h
+++ configs/dwmblocks/blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",        "networkTraffic.sh",    1,		         0},
-	{"",        "network.sh",       	1,		         0},
+	{"",        "networkTraffic.sh",    1,		         16},
+	{"",        "network.sh",       	1,		         4},
 	{"",        "resources.sh",       	1,		         0},
-	{"",        "volume.sh",       	    1,		         0},
-	{"",        "brightness.sh",       	1,		         0},
-	{"",        "battery.sh",       	1,		         0},
-	{"",        "time_date.sh",       	1,		         0},
+	{"",        "volume.sh",       	    1,		         10},
+	{"",        "brightness.sh",       	1,		         10},
+	{"",        "battery.sh",       	1,		         3},
+	{"",        "time_date.sh",       	1,		         1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
