30abf87f94879661afa2f2e6e99922669dbbf2d8
glowfi <86939591+glowfi@users.noreply.github.com>
2022-07-28 11:43:53 +0530

    minor color fix

--- configs/dmenu/config.h
+++ configs/dmenu/config.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#bbbbbb", "#32302F" },
 	[SchemeSel] = { "#eeeeee", "#458588" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
