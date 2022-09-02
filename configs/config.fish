f152eb3e8c3a561a15049a048280c5d1b0f05690
glowfi <86939591+glowfi@users.noreply.github.com>
2022-09-02 20:21:39 +0530

    removed bang bang fish

--- configs/config.fish
+++ configs/config.fish
@@ -401,36 +401,6 @@ function replaceWithSpecificWord
 
 end
 
-# Bang-Bang Function
-function __history_previous_command
-    switch (commandline -t)
-        case "!"
-            commandline -t $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
-
-function __history_previous_command_arguments
-    switch (commandline -t)
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-# Binding Bang-Bang Function
-bind ! __history_previous_command
-bind '$' __history_previous_command_arguments
-
-# set up the same key bindings for insert mode if using fish_vi_key_bindings
-if test "$fish_key_bindings" = fish_vi_key_bindings
-    bind --mode insert ! __history_previous_command
-    bind --mode insert '$' __history_previous_command_arguments
-end
 
 # ===================================================================
 #                            Theme
@@ -439,7 +409,7 @@ end
 
 function chooseTheme
     set choosen (printf "simple\nclassic\nminimal" | fzf)
-    sed -i "624s/.*/ $choosen/" ~/.config/fish/config.fish
+    sed -i "595s/.*/ $choosen/" ~/.config/fish/config.fish
 end
 
 function simple
