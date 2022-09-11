e23f54138f963ea2277fc56b0c42f41b5b754b63
glowfi <86939591+glowfi@users.noreply.github.com>
2022-09-11 17:06:21 +0530

    added to clipboard screenshot

--- scripts/windowshot.sh
+++ scripts/windowshot.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-#===============================================================================
-#         FILE: windowshot.sh
-#
-#  DESCRIPTION: Takes a screanshot using imagemagic and xprop
-#
-#       AUTHOR: Gavin Jaeger-Freeborn gavinfreeborn@gmail.com
-#      CREATED: Thu 28 Nov 2019 12:28:36 PM MST
-#      VERSION: 1.0
-#===============================================================================
-
-# This is bound to Shift+PrintScreen by default, requires maim. It lets you
-# choose the kind of screenshot to take, including copying the image or even
-# highlighting an area to copy. scrotcucks on suicidewatch right now.
 
 print_date() {
 	date '+%F_%T' | sed -e 's/:/-/g'
@@ -26,47 +13,45 @@ note() {
 
 mkdir -p "${SCREENSHOTDIR}"
 
-_end()
-{
+_end() {
 	note
 	xdg-open "${SCREENSHOTNAME}"
+	xclip -in -selection clipboard -target image/png ${SCREENSHOTNAME}
 	exit 0
 }
 
-region() { 
+region() {
 	killall unclutter
-	import "${SCREENSHOTNAME}" 
+	import "${SCREENSHOTNAME}"
 	setsid unclutter &
 	_end
 }
-window()
-{
+window() {
 	import -window "$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')" "${SCREENSHOTNAME}"
 	_end
 }
-root()
-{
+root() {
 	import -window root "${SCREENSHOTNAME}"
 	_end
 }
 
 # Default Prompt For Selection
-prompter(){
+prompter() {
 	case "$(printf 'a selected area\ncurrent window\nfull screen' | dmenu -l 6 -i -p 'Screenshot which area?')" in
-		"a selected area") region ;;
-		"current window") window ;;
-		"full screen") root ;;
-		*) exit ;;
+	"a selected area") region ;;
+	"current window") window ;;
+	"full screen") root ;;
+	*) exit ;;
 	esac
-_end
+	_end
 }
 
 while getopts cwr: o; do
 	case "$o" in
-		c) region;;
-		w) window ;;
-		r) root ;;
-		\?) printf 'Invalid option: -%s\n' "${o}" && exit ;;
+	c) region ;;
+	w) window ;;
+	r) root ;;
+	\?) printf 'Invalid option: -%s\n' "${o}" && exit ;;
 	esac
 done
 prompter
