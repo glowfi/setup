4d912dd2316688b9702d4a9d139865b8981ce097
glowfi <86939591+glowfi@users.noreply.github.com>
2022-10-04 14:22:03 +0530

    added kitty fixes

--- scripts/opa.sh
+++ scripts/opa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-line_check=$(sed -n '46p' ~/.config/kitty/kitty.conf)
+line_check=$(sed -n '47p' ~/.config/kitty/kitty.conf)
 
 if [[ "$line_check" == "## Tabs" ]]; then
 	cp -r ~/setup/configs/kitty/kitty.conf ~/.config/kitty/
 else
-	sed -i '45,49d' ~/.config/kitty/kitty.conf
+	sed -i '46,50d' ~/.config/kitty/kitty.conf
 fi
