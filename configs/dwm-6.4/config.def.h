26cd9867bb99e94976a1e14f8656531a8360e6a6
glowfi <86939591+glowfi@users.noreply.github.com>
2022-10-13 22:30:20 +0530

    added fixes

--- configs/dwm-6.4/config.def.h
+++ configs/dwm-6.4/config.def.h
@@ -1063,8 +1063,8 @@ static const Key keys[] = {
 	#if XRDB_PATCH && !BAR_VTCOLORS_PATCH
 	{ MODKEY|ShiftMask,             XK_F5,         xrdb,                   {.v = NULL } },
 	#endif // XRDB_PATCH
-	{ MODKEY,                       XK_t,          setlayout,              {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,          setlayout,              {.v = &layouts[1]} },
+	{ ALTKEY,                       XK_t,          setlayout,              {.v = &layouts[0]} },
+	{ ALTKEY,                       XK_f,          setlayout,              {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,          setlayout,              {.v = &layouts[2]} },
 	#if COLUMNS_LAYOUT
 	{ MODKEY,                       XK_c,          setlayout,              {.v = &layouts[3]} },
