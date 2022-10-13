71ab93304833848bcf853317eea6e330e67c4222
glowfi <86939591+glowfi@users.noreply.github.com>
2022-10-13 22:03:15 +0530

    added fixesv3

--- configs/dmenu/config.h
+++ configs/dmenu/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#32302F" },
-	[SchemeSel] = { "#eeeeee", "#433c3b" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
