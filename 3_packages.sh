799f8c5198b28c1ba2ebdc416b2a56a9e12f059c
glowfi <86939591+glowfi@users.noreply.github.com>
2023-01-26 01:08:41 +0530

    added files

--- 3_packages.sh
+++ 3_packages.sh
@@ -48,6 +48,7 @@ sudo pacman -S --noconfirm fish kitty ttf-fantasque-sans-mono man-db noto-fonts-
 sudo pacman -S --noconfirm alsa-utils alsa-plugins pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
 sudo pacman -S --noconfirm bluez bluez-utils bluedevil
 yay -S --noconfirm zramd nerd-fonts-fantasque-sans-mono ttf-ms-fonts ttf-vista-fonts
+sudo pacman -S --noconfirm android-tools scrcpy
 
 ### PACKAGES
 sudo pacman -S --noconfirm kdeconnect kcolorchooser
