27f61c7bf0cb91f52b69d415e7f07392d93498ea
glowfi <86939591+glowfi@users.noreply.github.com>
2023-02-12 01:41:08 +0530

    minor fix

--- scripts/tran.sh
+++ scripts/tran.sh
@@ -4,15 +4,19 @@ filez="$1"
 filezz="${filez#?}"
 curd=$(pwd)
 file="${curd}/${filezz}"
-echo "$file"
 
 up="$2"
 down="$3"
+codez="$4"
 qu=$((down + 1))
 
 if test -f "$file"; then
 	if [[ ! -z "${up}" && ! -z "${down}" ]]; then
-		code=$(trans --list-all | fzf | awk '{print $1}')
+		if [[ -z "${codez}" ]]; then
+			code=$(trans --list-all | fzf | awk '{print $1}')
+		else
+			code=${codez}
+		fi
 		text=$(sed -n "${up},${down}p;${qu}q" "$file")
 		if [[ ! -z "${code}" && ! -z "${text}" ]]; then
 			echo -e ""
