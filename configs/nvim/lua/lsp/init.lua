7188213d8d9bc1627d9eaf25ee2a34e099f2d171
glowfi <86939591+glowfi@users.noreply.github.com>
2023-02-13 13:50:28 +0530

    added fixes nvim

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -151,7 +151,7 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
 	settings = {
 		Lua = {
 			runtime = { version = "LuaJIT", path = runtime_path },
