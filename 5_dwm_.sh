032d4a5752d0dc2a7e303e04481e990743f56d9d
glowfi <86939591+glowfi@users.noreply.github.com>
2023-02-26 02:19:53 +0530

    added mimejs

--- 5_dwm_.sh
+++ 5_dwm_.sh
@@ -266,6 +266,18 @@ echo ""
 
 xdg-settings set default-web-browser brave-browser.desktop
 
+cd ~/.local/bin
+git clone https://github.com/karabaja4/mimejs
+cd mimejs
+rm -rf .git
+nodeLoc=$(whereis node | awk -F":" '{print $2}' | xargs)
+sed -i "1s|^.*$|\#\!$nodeLoc|" ./main.js
+rm mime.json
+wget https://0x0.st/Hskv.json -O mime.json
+sudo mv "/usr/bin/xdg-open" "/usr/bin/xdg-open.bak"
+sudo ln -s "${PWD}/main.js" "/usr/bin/xdg-open"
+sudo cp "${PWD}/mime.json" "/etc/mime.json"
+
 # REMOVE KWALLET
 
 sudo rm -rf /usr/share/dbus-1/services/org.kde.kwalletd5.service
--- 5_kde_.sh
+++ 5_kde_.sh
@@ -74,5 +74,17 @@ bash ./default-media-player.sh mpv.desktop
 rm -rf default-media-player.sh
 xdg-settings set default-web-browser brave-browser.desktop
 
+cd ~/.local/bin
+git clone https://github.com/karabaja4/mimejs
+cd mimejs
+rm -rf .git
+nodeLoc=$(whereis node | awk -F":" '{print $2}' | xargs)
+sed -i "1s|^.*$|\#\!$nodeLoc|" ./main.js
+rm mime.json
+wget https://0x0.st/Hskv.json -O mime.json
+sudo mv "/usr/bin/xdg-open" "/usr/bin/xdg-open.bak"
+sudo ln -s "${PWD}/main.js" "/usr/bin/xdg-open"
+sudo cp "${PWD}/mime.json" "/etc/mime.json"
+
 echo "Done seting default application!"
 echo ""
