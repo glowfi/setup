89c9cee0cd4f0fca1f16b9b8636877c5273f67d2
glowfi <86939591+glowfi@users.noreply.github.com>
2023-03-12 16:50:55 +0530

    added fixes

--- 3_packages.sh
+++ 3_packages.sh
@@ -46,7 +46,7 @@ sudo pacman -S --noconfirm xorg-server
 sudo pacman -S --noconfirm zip unzip unrar p7zip lzop
 sudo pacman -S --noconfirm fish kitty ttf-fantasque-sans-mono man-db noto-fonts-emoji noto-fonts
 sudo pacman -S --noconfirm alsa-utils alsa-plugins pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-sudo pacman -S --noconfirm bluez bluez-utils bluedevil
+sudo pacman -S --noconfirm bluez bluez-utils blueman
 yay -S --noconfirm ttf-fantasque-nerd ttf-ms-fonts ttf-vista-fonts
 sudo pacman -S --noconfirm android-tools scrcpy
 
