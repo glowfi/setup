cafc3fa085bcff26bfce60d14f80013f59c20d38
glowfi <86939591+glowfi@users.noreply.github.com>
2023-04-30 20:06:02 +0530

    fixed BSD Script

--- scripts/bsd.sh
+++ scripts/bsd.sh
@@ -6,11 +6,10 @@ sudo pkg upgrade
 
 ########### PACKAGES ###########
 
-sudo pkg install git nnn neovim firefox setsid wget gsed gawk
+sudo pkg install git nnn neovim firefox setsid wget gsed gawk xclip
 sudo pkg install fzf exa bottom fd-find bat gitui
-sudo pkg install nerd-fonts 
-sudo pkg install node npm
-sudo pkg install py39-pip 
+sudo pkg install nerd-fonts
+sudo pkg install py39-pip
 
 ########### CONFIGS ###########
 
@@ -19,8 +18,11 @@ cp -r ~/setup/scripts/send.sh ~/.local/bin/
 chmod +x ~/.local/bin/send.sh
 
 sudo pkg install tree-sitter meson ninja shfmt
-git clone https://github.com/nvim-lua/kickstart.nvim
-mv ./kickstart.nvim/ ~/.config/nvim/
+pip install neovim
+cd ~/.config
+mkdir kitty
+cd kitty
+cp -r ~/setup/configs/nvim/ .
 cd
 
 cd ~/.config
@@ -39,21 +41,18 @@ gawk -v line="14" -v text="$rep" '{
 mv output_file.txt kitty.conf
 cd
 
-
 rm -rf ~/.config/fish/config.fish
 cd ~/.config
 cp -r ~/setup/configs/config.fish ~/.config/fish/
 cd
 
-
 ########### JELLYFIN SERVER ###########
 
 fetch https://github.com/Thefrank/jellyfin-server-freebsd/releases/download/v10.8.9/jellyfinserver-10.8.9.pkg
 sudo pkg install jellyfinserver-10.8.9.pkg
-rm -rf jellyfinserver-10.8.9.pkg 
+rm -rf jellyfinserver-10.8.9.pkg
 sudo sysrc jellyfinserver_enable=TRUE
 sudo service jellyfinserver start
-
 
 ########### FIREFOX HARDENING ###########
 
