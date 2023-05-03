137bdded69258a990b336ce1aae7f97853fcc4cf
glowfi <86939591+glowfi@users.noreply.github.com>
2023-05-03 20:02:29 +0530

    removed a package

--- 4_cdx.sh
+++ 4_cdx.sh
@@ -170,8 +170,7 @@ echo ""
 
 # FZF TERMINAL INTEGRATION
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
+install "fzf" "pac"
 
 echo ""
 echo -------------------------------------------------------------------------------
