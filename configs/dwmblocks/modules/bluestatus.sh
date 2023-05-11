7b3e597d719b47caad8373704d0e6ce554067d86
glowfi <86939591+glowfi@users.noreply.github.com>
2023-05-11 17:21:12 +0530

    updated blueoth script

--- configs/dwmblocks/modules/bluestatus.sh
+++ configs/dwmblocks/modules/bluestatus.sh
@@ -2,12 +2,8 @@
 
 out=$(systemctl status bluetooth.service | head -2 | tail -1 | grep -Eo "enabled")
 
-Red=$'\e[1;31m'
-Green=$'\e[1;32m'
-Blue=$'\e[1;34m'
-
 if [[ "$out" = "" ]]; then
-	echo "$Red Off"
+	echo "🔴  Off"
 else
-	echo "$Blue On [$(bluetoothctl devices Connected | wc -l | xargs)]"
+	echo "🔵  On [$(bluetoothctl devices Connected | wc -l | xargs)]"
 fi
