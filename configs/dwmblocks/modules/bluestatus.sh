42cfd86a92956f2d4e62310ab81d2cef95742b0f
glowfi <86939591+glowfi@users.noreply.github.com>
2023-05-12 15:49:00 +0530

    updated a sciprt

--- configs/dwmblocks/modules/bluestatus.sh
+++ configs/dwmblocks/modules/bluestatus.sh
@@ -3,7 +3,7 @@
 out=$(systemctl status bluetooth.service | head -2 | tail -1 | grep -Eo "enabled")
 
 if [[ "$out" = "" ]]; then
-	echo "🔴  Off"
+	echo " Off"
 else
-	echo "🔵  On [$(bluetoothctl devices Connected | wc -l | xargs)]"
+	echo " On [$(bluetoothctl devices Connected | wc -l | xargs)]"
 fi
