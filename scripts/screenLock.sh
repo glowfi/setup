2bb2d3159625947423995618e0a5d5ceb4e85f7f
glowfi <86939591+glowfi@users.noreply.github.com>
2023-06-18 08:38:21 +0530

    made lockscreen more beautiful

--- scripts/screenLock.sh
+++ scripts/screenLock.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#ff00ffcc'
-TEXT='#ee00eeee'
-WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+print_date() {
+    date '+%F_%T' | sed -e 's/:/-/g'
+}
 
+SCREENSHOTDIR="${HOME}/.cache/ScreenShot"
+image="${SCREENSHOTDIR}/$(print_date).png"
+icon="/home/$USER/.misc/lock.png"
+hue=(-level "0%,100%,0.6")
+effect=(-filter Gaussian -resize 20% -define "filter:sigma=1.5" -resize 500.5%)
+
+
+# Create Directory if not present
+mkdir -p "${SCREENSHOTDIR}"
+
+root() {
+    import -window root "${image}"
+
+    rm -rf ~/.cache/output-0.png
+    rm -rf ~/.cache/output-1.png
+
+    convert "$image" "${hue[@]}" "${effect[@]}" -pointsize 26 -fill "#28282B" -gravity center -annotate +0+160 "LOCKED !" "$icon" -gravity center -composite "$image" "/home/$USER/.cache/output.png"
+
+    mv ~/.cache/output-0.png ~/.cache/out.png
+    rm -rf ~/.cache/output-1.png
+}
+
+
+# Take ScreenShot
+root
+
+
+# Start Python Script
 ~/.local/bin/screenLock.py &
 
-i3lock \
-	--insidever-color=$CLEAR \
-	--ringver-color=$VERIFYING \
-	\
-	--insidewrong-color=$CLEAR \
-	--ringwrong-color=$WRONG \
-	\
-	--inside-color=$BLANK \
-	--ring-color=$DEFAULT \
-	--line-color=$BLANK \
-	--separator-color=$DEFAULT \
-	\
-	--verif-color=$TEXT \
-	--wrong-color=$TEXT \
-	--time-color=$TEXT \
-	--date-color=$TEXT \
-	--layout-color=$TEXT \
-	--keyhl-color=$WRONG \
-	--bshl-color=$WRONG \
-	--screen 1 \
-	--blur 1 &
+# Start i3
+i3lock -i ~/.cache/out.png &
