050280f0301ffdc82ca986dfac91c69d416cab5b
glowfi <86939591+glowfi@users.noreply.github.com>
2023-06-22 07:01:47 +0530

    added some features

--- 3_packages.sh
+++ 3_packages.sh
@@ -117,18 +117,6 @@ sudo make clean install
 cd ..
 rm -rf tty-clock
 
-# ENABLE ZRAM
-
-echo ""
-echo "------------------------------------------------------------------------"
-echo "--------------Enabling ZRAM...------------------------------------------"
-echo "------------------------------------------------------------------------"
-echo ""
-install "zramd" "yay"
-sudo sed -i '2s/.*/ALGORITHM=zstd/' /etc/default/zramd
-sudo sed -i '8s/.*/MAX_SIZE=32768/' /etc/default/zramd
-sudo systemctl enable --now zramd
-
 # ADD FEATURES TO sudoers
 
 echo ""
@@ -139,23 +127,6 @@ echo ""
 
 sudo sed -i '71s/.*/Defaults insults/' /etc/sudoers
 echo "Done adding insults!"
-
-# SETUP APPARMOR
-
-echo ""
-echo "------------------------------------------------------------------------"
-echo "--------------Enabling APPARMOR...--------------------------------------"
-echo "------------------------------------------------------------------------"
-echo ""
-
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 lsm=landlock,lockdown,yama,apparmor,bpf"/' /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-install "apparmor" "pac"
-sudo systemctl enable --now apparmor.service
-
-# INCREASE VIRTUAL MEMORY
-
-sudo echo "vm.max_map_count=2147483642" | sudo tee -a /etc/sysctl.d/90-override.conf >/dev/null
 
 # THEMING GRUB
 
@@ -172,14 +143,6 @@ cp -r ~/setup/scripts/background-tela.jpg backgrounds/1080p/
 sudo ./install.sh -b -t tela
 cd ..
 rm -rf grub2-themes
-
-# SECURITY FEATURES
-
-touch ~/blacklist.conf
-echo "# Disable webcam
-blacklist uvcvideo" >>~/blacklist.conf
-sudo cp -r ~/blacklist.conf /etc/modprobe.d/
-rm ~/blacklist.conf
 
 echo ""
 echo "------------------------------------------------------------------------"
@@ -231,3 +194,152 @@ echo "--------------------------------------------------------------------------
 echo ""
 sudo usermod --shell /bin/fish $1
 echo "Changed default shell!"
+
+
+
+# PERFORMANCE AND SECURITY
+
+echo ""
+echo "------------------------------------------------------------------------------"
+echo "--------------CONFIGURING PERFORMANCE AND SECURITY...-------------------------"
+echo "------------------------------------------------------------------------------"
+echo ""
+
+# ENABLE ZRAM
+
+install "zramd" "yay"
+sudo sed -i '2s/.*/ALGORITHM=zstd/' /etc/default/zramd
+sudo sed -i '8s/.*/MAX_SIZE=32768/' /etc/default/zramd
+sudo systemctl enable --now zramd
+
+
+# SETUP APPARMOR
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 lsm=landlock,lockdown,yama,apparmor,bpf"/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+install "apparmor" "pac"
+sudo systemctl enable --now apparmor.service
+
+# INCREASE VIRTUAL MEMORY
+
+sudo echo "vm.max_map_count=2147483642" | sudo tee -a /etc/sysctl.d/90-override.conf >/dev/null
+
+# NETWORKING SETTINGS
+
+sudo echo 'DNS=9.9.9.9
+DNSOverTLS=yes
+LLMNR=no' | sudo tee -a /etc/systemd/resolved.conf >/dev/null
+
+# SECURITY FEATURES
+
+sudo echo "# Disable webcam
+blacklist uvcvideo" | sudo tee -a /etc/modprobe.d/blacklist.conf >/dev/null
+
+# PERFORMANCE AND SECURITY SETTINGS
+
+sudo sed -i 's/^umask.*/umask\ 077/' /etc/profile
+sudo echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/bbr.conf >/dev/null
+sudo echo "write-cache" | sudo tee -a /etc/apparmor/parser.conf >/dev/null
+
+echo "# The swappiness sysctl parameter represents the kernel's preference (or avoidance) of swap space. Swappiness can have a value between 0 and 100, the default value is 60.
+# A low value causes the kernel to avoid swapping, a higher value causes the kernel to try to use swap space. Using a low value on sufficient memory is known to improve responsiveness on many systems.
+vm.swappiness=10
+
+# The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache).
+# Lowering it from the default value of 100 makes the kernel less inclined to reclaim VFS cache (do not set it to 0, this may produce out-of-memory conditions)
+vm.vfs_cache_pressure=50
+
+# This action will speed up your boot and shutdown, because one less module is loaded. Additionally disabling watchdog timers increases performance and lowers power consumption
+# Disable NMI watchdog
+#kernel.nmi_watchdog = 0
+
+# Contains, as a percentage of total available memory that contains free pages and reclaimable
+# pages, the number of pages at which a process which is generating disk writes will itself start
+# writing out dirty data (Default is 20).
+vm.dirty_ratio = 5
+
+# Contains, as a percentage of total available memory that contains free pages and reclaimable
+# pages, the number of pages at which the background kernel flusher threads will start writing out
+# dirty data (Default is 10).
+vm.dirty_background_ratio = 5
+
+# This tunable is used to define when dirty data is old enough to be eligible for writeout by the
+# kernel flusher threads.  It is expressed in 100'ths of a second.  Data which has been dirty
+# in-memory for longer than this interval will be written out next time a flusher thread wakes up
+# (Default is 3000).
+#vm.dirty_expire_centisecs = 3000
+
+# The kernel flusher threads will periodically wake up and write old data out to disk.  This
+# tunable expresses the interval between those wakeups, in 100'ths of a second (Default is 500).
+vm.dirty_writeback_centisecs = 1500
+
+# Enable the sysctl setting kernel.unprivileged_userns_clone to allow normal users to run unprivileged containers.
+kernel.unprivileged_userns_clone=1
+
+# To hide any kernel messages from the console
+kernel.printk = 3 3 3 3
+
+# Restricting access to kernel logs
+kernel.dmesg_restrict = 1
+
+# Restricting access to kernel pointers in the proc filesystem
+kernel.kptr_restrict = 2
+
+# Disable Kexec, which allows replacing the current running kernel.
+kernel.kexec_load_disabled = 1
+
+# Increasing the size of the receive queue.
+# The received frames will be stored in this queue after taking them from the ring buffer on the network card.
+# Increasing this value for high speed cards may help prevent losing packets:
+net.core.netdev_max_backlog = 16384
+
+# Increase the maximum connections
+#The upper limit on how many connections the kernel will accept (default 128):
+net.core.somaxconn = 8192
+
+# Increase the memory dedicated to the network interfaces
+# The default the Linux network stack is not configured for high speed large file transfer across WAN links (i.e. handle more network packets) and setting the correct values may save memory resources:
+net.core.rmem_default = 1048576
+net.core.rmem_max = 16777216
+net.core.wmem_default = 1048576
+net.core.wmem_max = 16777216
+net.core.optmem_max = 65536
+net.ipv4.tcp_rmem = 4096 1048576 2097152
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+
+# Enable TCP Fast Open
+# TCP Fast Open is an extension to the transmission control protocol (TCP) that helps reduce network latency
+# by enabling data to be exchanged during the sender’s initial TCP SYN [3].
+# Using the value 3 instead of the default 1 allows TCP Fast Open for both incoming and outgoing connections:
+net.ipv4.tcp_fastopen = 3
+
+# Enable BBR
+# The BBR congestion control algorithm can help achieve higher bandwidths and lower latencies for internet traffic
+net.core.default_qdisc = cake
+net.ipv4.tcp_congestion_control = bbr
+
+# TCP SYN cookie protection
+# Helps protect against SYN flood attacks. Only kicks in when net.ipv4.tcp_max_syn_backlog is reached:
+net.ipv4.tcp_syncookies = 1
+
+# Protect against tcp time-wait assassination hazards, drop RST packets for sockets in the time-wait state. Not widely supported outside of Linux, but conforms to RFC:
+net.ipv4.tcp_rfc1337 = 1
+
+# By enabling reverse path filtering, the kernel will do source validation of the packets received from all the interfaces on the machine. This can protect from attackers that are using IP spoofing methods to do harm.
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.rp_filter = 1
+
+# Disable ICMP redirects
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+
+# To use the new FQ-PIE Queue Discipline (>= Linux 5.6) in systems with systemd (>= 217), will need to replace the default fq_codel.
+net.core.default_qdisc = fq_pie" | sudo tee -a /etc/sysctl.d/99-sysctl-performance-tweaks.conf >/dev/null
