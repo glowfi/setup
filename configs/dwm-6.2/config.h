70a998f4800872372bf1e5d6cde2909423335ffa
glowfi <86939591+glowfi@users.noreply.github.com>
2023-07-02 11:00:47 +0530

    fixed DWM colorscheme

--- configs/dwm-6.2/config.h
+++ configs/dwm-6.2/config.h
@@ -12,11 +12,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fantasque Sans Mono Bold:size=13","JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#433c3b";
+static const char col_cyan[]        = "#3c3836";
 static const char col_trans[]       = "#FF0000";
 static const char *colors[][3]      = {
    /*               fg         bg         border   */ 
