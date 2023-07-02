151b6ea33e21452b786f7da6a29885fc73746089
glowfi <86939591+glowfi@users.noreply.github.com>
2023-07-02 10:58:06 +0530

    added gruvbox theme dmenu

--- configs/dmenu/config.h
+++ configs/dmenu/config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#32302F" },
-	[SchemeSel] = { "#eeeeee", "#433c3b" },
+	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#fabd2f", "#3c3836" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
