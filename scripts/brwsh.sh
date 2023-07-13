cbb2706e4569c036f62fff0bb7fe465c4a196afc
glowfi <86939591+glowfi@users.noreply.github.com>
2023-07-13 15:00:23 +0530

    added fixes

--- scripts/brwsh.sh
+++ scripts/brwsh.sh
@@ -28,62 +28,20 @@ sleep 6
 cat $HOME/setup/configs/brave/settings.json > "$HOME/.config/BraveSoftware/Brave-Browser/Tmp/Preferences"
 
 
+######## Librewolf ########
 
-######## Firefox ########
+rm -rf $HOME/.librewolf/
 
-rm -rf $HOME/.mozilla
-sudo rm -rf /usr/lib/firefox/distribution
+###### Start Librewolf ######
 
-### Policies
-
-sudo mkdir -p /usr/lib/firefox/distribution/
-sudo touch /usr/lib/firefox/distribution/policies.json
-echo '
-{
-	"policies": {
-		"CaptivePortal": false,
-		"DisableFirefoxAccounts": true,
-		"DisableFirefoxStudies": true,
-		"DisablePocket": true,
-		"DisableTelemetry": true,
-		"Extensions": {
-			"Install": [
-				"https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi",
-				"https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi"
-				]
-		},
-            "FirefoxHome": {
-			"Search": true,
-			"TopSites": false,
-			"SponsoredTopSites": false,
-			"Highlights": false,
-			"Pocket": false,
-			"SponsoredPocket": false,
-			"Snippets": false,
-			"Locked": false
-		},
-		"NetworkPrediction": false,
-		"OverrideFirstRunPage": "about:home",
-		"UserMessaging": {
-			"WhatsNew": false,
-			"ExtensionRecommendations": false,
-			"FeatureRecommendations": false,
-			"SkipOnboarding": false
-		},
-		"NoDefaultBookmarks":true
-	}
-}'| sudo tee -a /usr/lib/firefox/distribution/policies.json >/dev/null
-
-###### Start Firefox ######
-
-firefox &
+librewolf &
 sleep 6
-killall firefox
+killall librewolf
 
 ###### Arkenfox Profile ######
 
 # Get Default-release Location
-findLocation=$(find ~/.mozilla/firefox/ | grep -E "default-release" | head -1)
+findLocation=$(find ~/.librewolf/ | grep -E "default-default" | head -1)
 
 # Go to default-release profile
 cd "$findLocation"
@@ -115,3 +73,90 @@ echo 'user_pref("general.smoothScroll.stopDecelerationWeighting",             1.
 echo 'user_pref("mousewheel.default.delta_multiplier_y",                      300);' >> user.js
 
 cd
+
+# ######## Firefox ########
+
+# rm -rf $HOME/.mozilla
+# sudo rm -rf /usr/lib/firefox/distribution
+
+# ### Policies
+
+# sudo mkdir -p /usr/lib/firefox/distribution/
+# sudo touch /usr/lib/firefox/distribution/policies.json
+# echo '
+# {
+# 	"policies": {
+# 		"CaptivePortal": false,
+# 		"DisableFirefoxAccounts": true,
+# 		"DisableFirefoxStudies": true,
+# 		"DisablePocket": true,
+# 		"DisableTelemetry": true,
+# 		"Extensions": {
+# 			"Install": [
+# 				"https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi",
+# 				"https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi"
+# 				]
+# 		},
+#             "FirefoxHome": {
+# 			"Search": true,
+# 			"TopSites": false,
+# 			"SponsoredTopSites": false,
+# 			"Highlights": false,
+# 			"Pocket": false,
+# 			"SponsoredPocket": false,
+# 			"Snippets": false,
+# 			"Locked": false
+# 		},
+# 		"NetworkPrediction": false,
+# 		"OverrideFirstRunPage": "about:home",
+# 		"UserMessaging": {
+# 			"WhatsNew": false,
+# 			"ExtensionRecommendations": false,
+# 			"FeatureRecommendations": false,
+# 			"SkipOnboarding": false
+# 		},
+# 		"NoDefaultBookmarks":true
+# 	}
+# }'| sudo tee -a /usr/lib/firefox/distribution/policies.json >/dev/null
+
+# ###### Start Firefox ######
+
+# firefox &
+# sleep 6
+# killall firefox
+
+# ###### Arkenfox Profile ######
+
+# # Get Default-release Location
+# findLocation=$(find ~/.mozilla/firefox/ | grep -E "default-release" | head -1)
+
+# # Go to default-release profile
+# cd "$findLocation"
+
+# # User CSS
+# mkdir chrome
+# cd chrome
+# cd ..
+
+# # Get Arkenfox user.js
+# wget https://raw.githubusercontent.com/arkenfox/user.js/master/user.js -O user.js
+
+# # Settings
+# echo -e "\n" >> user.js
+# echo "// ****** OVERRIDES ******" >> user.js
+# echo "" >> user.js
+# echo 'user_pref("keyword.enabled", true);' >> user.js
+# echo "user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);" >> user.js
+# echo 'user_pref("general.smoothScroll",                                       true);'>> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);'  >> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.enabled",                    true);'>> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",  600);' >> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      650);' >> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         25);'  >> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",      2.0);' >> user.js
+# echo 'user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     250);' >> user.js
+# echo 'user_pref("general.smoothScroll.currentVelocityWeighting",              1.0);' >> user.js
+# echo 'user_pref("general.smoothScroll.stopDecelerationWeighting",             1.0);' >> user.js
+# echo 'user_pref("mousewheel.default.delta_multiplier_y",                      300);' >> user.js
+
+# cd
