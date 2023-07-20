6cd7dcfb1d4f17fc6c5c48fbffe37e171167472e
glowfi <86939591+glowfi@users.noreply.github.com>
2023-07-21 02:11:04 +0530

    udpated a script

--- scripts/blank.sh
+++ scripts/blank.sh
@@ -5,7 +5,8 @@ tmpfile="$HOME/.cache/blank"
 generate() {
     echo "generate!"
     touch "$tmpfile"
-    ps aux | grep -E "wall.sh" | head | awk '{print $2}'|head -1|xargs -I {} kill -9 "{}";killall wall.sh
+    ps aux | grep -E "wall.sh" | head | awk '{print $2}'|head -1|xargs -I {} kill -9 "{}";
+    ps aux | grep -E "xautolock" | head | awk '{print $2}'|head -1|xargs -I {} kill -9 "{}";
     convert -size 1920x1080 xc:black png:- | feh --bg-fill -
     xdotool key alt+b
 }
@@ -13,6 +14,7 @@ generate() {
 restore(){
     echo "restore!"
     sh $HOME/.local/bin/wall.sh &
+    xautolock -time 10 -locker $HOME/.local/bin/screenlocker &
     rm -rf "$tmpfile"
     xdotool key alt+b
 }
