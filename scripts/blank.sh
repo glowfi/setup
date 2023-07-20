9785306be2e43200a4dc31806201c122f4dac1ad
glowfi <86939591+glowfi@users.noreply.github.com>
2023-07-20 13:40:53 +0530

    minor change

--- scripts/blank.sh
+++ scripts/blank.sh
@@ -7,12 +7,14 @@ generate() {
     touch "$tmpfile"
     ps aux | grep -E "wall.sh" | head | awk '{print $2}'|head -1|xargs -I {} kill -9 "{}";killall wall.sh
     convert -size 1920x1080 xc:black png:- | feh --bg-fill -
+    xdotool key alt+b
 }
 
 restore(){
     echo "restore!"
     sh $HOME/.local/bin/wall.sh &
     rm -rf "$tmpfile"
+    xdotool key alt+b
 }
 
 
