c39c45e0f1ef5183cb3e7a19230aa7be1567da34
glowfi <86939591+glowfi@users.noreply.github.com>
2023-09-02 20:29:05 +0530

    added fixesv3

--- 3_0_packages.sh
+++ 3_0_packages.sh
@@ -84,6 +84,7 @@ sudo gpasswd -a $USER plugdev
 install "android-tools scrcpy" "pac"
 install "kdeconnect kcolorchooser" "pac"
 install "mediainfo perl-image-exiftool" "pac"
+install "inotify-tools" "pac"
 
 ### CORE (Use old package version)
 mkdir test
