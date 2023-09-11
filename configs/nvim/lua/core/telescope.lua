a2c720e1d2c3f0bcc837edb9f7824921d9261140
glowfi <86939591+glowfi@users.noreply.github.com>
2023-09-11 08:02:09 +0530

    added some update to nvim

--- configs/nvim/lua/core/telescope.lua
+++ configs/nvim/lua/core/telescope.lua
@@ -1,4 +1,9 @@
 -- Keymappings
+vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>/", function()
+	require("telescope.builtin").current_buffer_fuzzy_find()
+end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
@@ -59,3 +64,6 @@ telescope.setup({
 		},
 	},
 })
+
+-- Enable telescope fzf native, if installed
+pcall(require("telescope").load_extension, "fzf")
