c4e00e2a8a29f20cdbc6a362b40d7d2ac587dadb
glowfi <86939591+glowfi@users.noreply.github.com>
2023-09-23 23:55:00 +0530

    minor fix

--- 3_2_performance_security.sh
+++ 3_2_performance_security.sh
@@ -103,7 +103,7 @@ sudo echo "tcp_bbr" | sudo tee -a /etc/modules-load.d/bbr.conf >/dev/null
 
 echo "# The swappiness sysctl parameter represents the kernel's preference (or avoidance) of swap space. Swappiness can have a value between 0 and 100, the default value is 60.
 # A low value causes the kernel to avoid swapping, a higher value causes the kernel to try to use swap space. Using a low value on sufficient memory is known to improve responsiveness on many systems.
-vm.swappiness=30
+vm.swappiness=40
 
 # The value controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects (VFS cache).
 # Lowering it from the default value of 100 makes the kernel less inclined to reclaim VFS cache (do not set it to 0, this may produce out-of-memory conditions)
@@ -112,7 +112,7 @@ vm.vfs_cache_pressure=50
 # Contains, as a percentage of total available memory that contains free pages and reclaimable
 # pages, the number of pages at which a process which is generating disk writes will itself start
 # writing out dirty data (Default is 20).
-vm.dirty_ratio = 5
+vm.dirty_ratio = 10
 
 # Contains, as a percentage of total available memory that contains free pages and reclaimable
 # pages, the number of pages at which the background kernel flusher threads will start writing out
