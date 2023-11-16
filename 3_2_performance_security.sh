1e7f5e4a44268060aeedc0ee1927087d401583ba
glowfi <86939591+glowfi@users.noreply.github.com>
2023-11-16 21:11:14 +0530

    minor fix

--- 3_2_performance_security.sh
+++ 3_2_performance_security.sh
@@ -12,6 +12,20 @@ echo "--------------CONFIGURING PERFORMANCE AND SECURITY...---------------------
 echo "------------------------------------------------------------------------------"
 echo ""
 
+# ===================== XORG Dependent ===================================
+
+# Enable tap to click
+[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+	# Enable left mouse button by tapping
+	Option "Tapping" "on"
+EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
+
+# ===================== END Dependent ====================================
+
 # ENABLE ZRAM
 
 if [[ "$1" != "systemD" ]]; then
