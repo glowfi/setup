58652b69d35b889dce4f5456c743d7de37fe2bd8
glowfi <86939591+glowfi@users.noreply.github.com>
2023-11-26 21:40:15 +0530

    added models back

---
+++ configs/nvim/lua/core/gen.lua
@@ -0,0 +1,10 @@
+-- Keymappings
+vim.keymap.set("v", "<leader>]", ":Gen<CR>")
+vim.keymap.set("n", "<leader>]", ":Gen<CR>")
+
+-- Setup
+require("gen").setup({
+	opts = {
+		model = "mistral",
+	},
+})
