f10a12d2037dfc095dbcc48e89b781d97b26d060
glowfi <86939591+glowfi@users.noreply.github.com>
2024-03-01 00:06:03 +0530

    added minor fix

--- scripts/virtualization/vm_manager.sh
+++ scripts/virtualization/vm_manager.sh
@@ -28,10 +28,17 @@ update() {
 # Perform the choosen task
 if [[ "$choice1" != "" ]]; then
 	# Choose what to do
-	choice2=$(echo -e "1.Start VM (UEFI)\n2.Fallback Start Script(UEFI)\n3.Fallback Start Script (Legacy BIOS)\n4.Close VM\n5.Delete VM\n6.Reconfigure VM\n7.Update Scripts" | fzf | awk -F"." '{print $1}')
+	choice2=$(echo -e "1.Start VM (UEFI)
+2.Start VM (UEFI with SecureBoot)
+3.Fallback Start Script(UEFI)
+4.Fallback Start Script (Legacy BIOS)
+5.Close VM
+6.Delete VM
+7.Reconfigure VM
+8.Update Scripts" | fzf | awk -F"." '{print $1}')
 	cd "${VM_PATH}/${choice1}"
 
-	if [[ "$choice2" != "" && "$choice2" == "5" ]]; then
+	if [[ "$choice2" != "" && "$choice2" == "6" ]]; then
 		cd
 		rm -rf "${VM_PATH}/${choice1}"
 	else
@@ -39,14 +46,16 @@ if [[ "$choice1" != "" ]]; then
 			if [[ "$choice2" == "1" ]]; then
 				./start.sh
 			elif [[ "$choice2" == "2" ]]; then
-				./fallback-start.sh
+				./start_secboot.sh
 			elif [[ "$choice2" == "3" ]]; then
-				./fallback-start-BIOS.sh
+				./fallback-start.sh
 			elif [[ "$choice2" == "4" ]]; then
+				./fallback-start-BIOS.sh
+			elif [[ "$choice2" == "5" ]]; then
 				./clean.sh
-			elif [[ "$choice2" == "6" ]]; then
-				reconfigure "${VM_PATH}/${choice1}" "${choice1}"
 			elif [[ "$choice2" == "7" ]]; then
+				reconfigure "${VM_PATH}/${choice1}" "${choice1}"
+			elif [[ "$choice2" == "8" ]]; then
 				update "${VM_PATH}/${choice1}" "${choice1}"
 			fi
 		fi
