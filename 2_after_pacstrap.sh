ab4640d47654715d52650891c38a7deb94d3fdc4
glowfi <86939591+glowfi@users.noreply.github.com>
2024-03-21 17:29:29 +0530

    added a package

--- 2_after_pacstrap.sh
+++ 2_after_pacstrap.sh
@@ -189,7 +189,7 @@ driveType=$(sed -n '4p' <"$CONFIG_FILE")
 
 install "os-prober grub efibootmgr ntfs-3g" "pac"
 install "cracklib" "pac"
-install "networkmanager network-manager-applet wireless_tools wpa_supplicant net-tools dnsutils" "pac"
+install "networkmanager network-manager-applet wireless_tools wpa_supplicant net-tools dnsutils usbutils" "pac"
 install "dialog mtools dosfstools gptfdisk" "pac"
 install "rsync reflector wget" "pac"
 install "lsof strace bc" "pac"
