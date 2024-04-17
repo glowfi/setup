4f36b90e1564801384cabc986a858b632fd2e08f
glowfi <86939591+glowfi@users.noreply.github.com>
2024-04-17 19:12:52 +0530

    minor fix

--- 4_cdx.sh
+++ 4_cdx.sh
@@ -75,8 +75,8 @@ nohup ollama serve &
 rm nohup.out
 nohup ollama serve &
 rm nohup.out
-ollama pull mistral && ollama pull mistral && ollama pull mistral
-ollama pull zephyr:7b-beta && ollama pull zephyr:7b-beta && ollama pull zephyr:7b-beta
+ollama pull mistral || ollama pull mistral || ollama pull mistral
+ollama pull zephyr:7b-beta || ollama pull zephyr:7b-beta || ollama pull zephyr:7b-beta
 ps aux | grep -i 'ollama' | awk '{print $2}' | xargs -ro kill -9
 
 # ======================================================= END ======================================================================================
