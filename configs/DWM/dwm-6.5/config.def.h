f73864eb0984408056279983bd01f0864e6fafa6
glowfi <86939591+glowfi@users.noreply.github.com>
2024-05-04 19:16:22 +0530

    minor fix

--- configs/DWM/dwm-6.5/config.def.h
+++ configs/DWM/dwm-6.5/config.def.h
@@ -497,8 +497,8 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
+	/* RULE(.class = "Gimp", .tags = 1 << 4) */
+	/* RULE(.class = "Firefox", .tags = 1 << 7) */
 	#if RENAMED_SCRATCHPADS_PATCH
 	RULE(.instance = "spterm", .scratchkey = 's', .isfloating = 1)
 	#elif SCRATCHPADS_PATCH
