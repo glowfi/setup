aa21e1c1fec1305b0e2b0af635be7311dabd592e
glowfi <86939591+glowfi@users.noreply.github.com>
2024-05-06 03:45:05 +0530

    minor fix

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -1,7 +1,7 @@
 -- Settings
 -- Lsp Diagnostic signs
 vim.fn.sign_define("DiagnosticSignError", { texthl = "DiagnosticSignError", text = "󰅚", numhl = "" })
-vim.fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "" })
+vim.fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "" })
 vim.fn.sign_define("DiagnosticSignHint", { texthl = "DiagnosticSignHint", text = "󰌶", numhl = "" })
 vim.fn.sign_define("DiagnosticSignInfo", { texthl = "DiagnosticSignInfo", text = "", numhl = "" })
 
