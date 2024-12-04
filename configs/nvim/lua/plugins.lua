4d7a64432fb876610dc3cf73ef944a4ec7c7fc4f
glowfi <86939591+glowfi@users.noreply.github.com>
2024-12-04 23:55:59 +0530

    markdown previw fix plugin nvim

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -334,10 +334,11 @@ return require("lazy").setup({
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
 		end,
+		ft = { "markdown" },
 	},
 
 	{
