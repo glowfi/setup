d852f31d5d6caa2824a1133a7ae73aace5d2136e
glowfi <86939591+glowfi@users.noreply.github.com>
2025-03-31 19:44:46 +0530

    fix nvim git signs colorscheme

--- configs/nvim/lua/core/colorscheme.lua
+++ configs/nvim/lua/core/colorscheme.lua
@@ -22,9 +22,9 @@ vim.cmd("hi! DiagnosticSignHint  ctermbg=none guibg=none guifg=#8ec07c")
 vim.cmd("hi! DiagnosticSignInfo  ctermbg=none guibg=none guifg=#83a598")
 
 -- Gitsigns
--- vim.cmd("hi! GitSignsAdd     ctermbg=none guibg=none guifg=#b8bb26")
--- vim.cmd("hi! GitSignsChange  ctermbg=none guibg=none guifg=#83a598")
--- vim.cmd("hi! GitSignsDelete  ctermbg=none guibg=none guifg=#fb4934")
+vim.cmd("hi! GitSignsAdd     ctermbg=none guibg=none guifg=#b8bb26")
+vim.cmd("hi! GitSignsChange  ctermbg=none guibg=none guifg=#83a598")
+vim.cmd("hi! GitSignsDelete  ctermbg=none guibg=none guifg=#fb4934")
 
 -- CursorLine
 -- vim.cmd("set cursorline")
