4f3cae1b595f4a7bcfc21cde1ae0e7620435babf
glowfi <86939591+glowfi@users.noreply.github.com>
2025-10-12 19:06:25 +0530

    fix nodejs version extraction logic

--- 4_cdx.sh
+++ 4_cdx.sh
@@ -89,7 +89,7 @@ echo ""
 
 # DOWNLOAD NODEJS
 
-set ver (curl https://nodejs.org/en | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep -E "node-v"|head -1|cut -d"/" -f5|xargs)
+set ver (curl https://nodejs.org/en/download | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep -i "releases/tag" | cut -d"/" -f8| xargs)
 wget "https://nodejs.org/dist/$ver/node-$ver-linux-x64.tar.xz" -O $HOME/node.tar.xz
 tar -xf $HOME/node.tar.xz -C $HOME
 rm -rf $HOME/node.tar.xz
