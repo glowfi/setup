26e97f5c7aa0931188b5439c4a042ccf07f5dd3e
glowfi <86939591+glowfi@users.noreply.github.com>
2025-12-24 19:24:42 +0530

    install k9s

--- 4_cdx.sh
+++ 4_cdx.sh
@@ -462,6 +462,7 @@ go install github.com/jesseduffield/lazydocker@latest
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl $HOME/.local/bin
+install "k9s" "pac"
 
 ### DOWNLOAD NEOVIM
 
