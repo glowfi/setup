988ee79cd3595ab13f3a78d7e2be0d78e362c61b
glowfi <86939591+glowfi@users.noreply.github.com>
2026-01-18 17:32:45 +0530

    minor fix to dwm

--- configs/DWM/dwm-6.5/config.h
+++ configs/DWM/dwm-6.5/config.h
@@ -210,7 +210,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key            function                argument */
-	/* { MODKEY,                       XK_p,          spawn,                  {.v = dmenucmd } }, */
+	{ MODKEY,                       XK_d,          spawn,                  {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,     spawn,                  {.v = termcmd } },
 	{ ALTKEY,                       XK_b,          togglebar,              {0} },
 	{ MODKEY,                       XK_j,          focusstack,             {.i = +1 } },
