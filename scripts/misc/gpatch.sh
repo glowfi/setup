78eff9e9d4589d2dda5e391b559e17a94e209153
glowfi <86939591+glowfi@users.noreply.github.com>
2026-01-22 20:59:02 +0530

    add gamescope to gpatch

--- scripts/misc/gpatch.sh
+++ scripts/misc/gpatch.sh
@@ -26,7 +26,7 @@ for ((i = 0; i < 5; i++)); do
 	echo "y"
 	echo "y"
 	echo "y"
-) | sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 vulkan-icd-loader lib32-vulkan-icd-loader && break
+) | sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 vulkan-icd-loader lib32-vulkan-icd-loader gamescope && break
 done
 for i in {1..5}; do sudo pacman -S --noconfirm extra/mesa-utils vulkan-tools && break || sleep 1; done
 for i in {1..5}; do yay -S --noconfirm protontricks-git && break || sleep 1; done
