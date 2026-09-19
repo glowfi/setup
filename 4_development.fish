cff4ac26177677783cf07d48c912976376fab718
glowfi <86939591+glowfi@users.noreply.github.com>
2026-09-19 21:46:14 +0530

    update installation method of zig and zls

--- 4_development.fish
+++ 4_development.fish
@@ -62,7 +62,19 @@ end
 curl -fsSL https://bun.sh/install | bash
 
 # Zig
-install "zig zls" pac
+header "Installing Zig"
+
+set arch (uname -m)-linux
+set ver (curl -s https://ziglang.org/download/index.json | jq -r 'del(.master) | keys[]' | sort -V | tail -1)
+set zig_url https://ziglang.org/download/$ver/zig-$arch-$ver.tar.xz
+set zls_url (curl -s "https://releases.zigtools.org/v1/zls/select-version?zig_version=$ver&compatibility=only-runtime" | jq -r ".\"$arch\".tarball")
+
+mkdir -p ~/.local/bin ~/.local/lib
+set tmp (mktemp -d)
+curl -sL $zig_url | tar -xJ -C $tmp --strip-components=1
+mv $tmp/zig ~/.local/bin/zig
+rm -rf ~/.local/lib/zig; and mv $tmp/lib ~/.local/lib/zig
+curl -sL $zls_url | tar -xJ -C ~/.local/bin zls
 
 # Rust
 header "Installing Rust"
